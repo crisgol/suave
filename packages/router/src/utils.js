ff5b3d55555c9eba709e51ffa626de39a21423cc
Tim Hall <tim.hall.engr@gmail.com>
2019-01-24 16:36:59 -0500

    Continue fleshing out routing

--- packages/router/src/utils.js
+++ packages/router/src/utils.js
@@ -8,3 +8,11 @@ export function without(object, excluded_keys) {
 
   return result;
 }
+
+export function isComponent(value) {
+  return value && typeof value.prototype.$destroy === 'function';
+}
+
+export function isPromise(value) {
+  return value && typeof value.then === 'function';
+}
